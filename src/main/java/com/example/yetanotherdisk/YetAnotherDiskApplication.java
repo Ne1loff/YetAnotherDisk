662486a437aa23130090cec3d7b2a9ee4728a9d1
xinik <sergey20160@gmail.com>
2022-09-10 23:41:43 +0300

    Initial commit

---
+++ src/main/java/com/example/yetanotherdisk/YetAnotherDiskApplication.java
@@ -0,0 +1,13 @@
+package com.example.yetanotherdisk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YetAnotherDiskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YetAnotherDiskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/yetanotherdisk/YetAnotherDiskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.yetanotherdisk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YetAnotherDiskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
