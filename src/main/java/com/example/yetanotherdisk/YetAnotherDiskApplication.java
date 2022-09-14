434cc21af148c82d3303af8361127f7d830bbd6f
xinik <sergey20160@gmail.com>
2022-09-14 22:50:54 +0300

    Add mapper configuration bean

--- src/main/java/com/example/yetanotherdisk/YetAnotherDiskApplication.java
+++ src/main/java/com/example/yetanotherdisk/YetAnotherDiskApplication.java
@@ -1,7 +1,12 @@
 package com.example.yetanotherdisk;
 
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import static org.modelmapper.config.Configuration.AccessLevel.PRIVATE;
 
 @SpringBootApplication
 public class YetAnotherDiskApplication {
@@ -10,4 +15,13 @@ public class YetAnotherDiskApplication {
         SpringApplication.run(YetAnotherDiskApplication.class, args);
     }
 
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper mapper = new ModelMapper();
+        mapper.getConfiguration()
+                .setMatchingStrategy(MatchingStrategies.STRICT)
+                .setFieldMatchingEnabled(true)
+                .setFieldAccessLevel(PRIVATE);
+        return mapper;
+    }
 }
